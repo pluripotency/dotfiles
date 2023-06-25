e41a6a7b9f2cf197de9d8d7a9f7d060854577630
Masayoshi Kokuryo <masakoku@gmail.com>
2023-06-25 11:47:08 +0900

    plugins setting for nvks

--- nvks/lua/custom/plugins/init.lua
+++ nvks/lua/custom/plugins/init.lua
@@ -2,7 +2,6 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-vim.keymap.set('n', '<leader>n', ":Neotree toggle<CR>", { desc = '[N]eotree toggle' })
 
 -- Better Window Navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = '' })
@@ -17,26 +16,18 @@ vim.keymap.set("n", "<C-Right>", "<C-w><<CR>", { desc = '' })
 -- Split Window
 vim.keymap.set("n", "ss",  ":split<Return><C-w>w", { desc = '' })
 vim.keymap.set("n", "sv",  ":vsplit<Return><C-w>w", { desc = '' })
-
 -- Terminal
 vim.keymap.set("n", "<C-t>",  ":split<CR><C-w>j:resize 20<CR>:term<CR>", { desc = '' })
 vim.keymap.set("t", "<ESC>",  "<C-\\><C-n>", { desc = '' })
 
--- Edit Config
-vim.keymap.set("n", "<Leader>.",  ":<C-u>edit $HOME/dotfiles/lua<Enter>", { desc = '' })
-vim.keymap.set("n", "<Leader>s.", ":<C-u>source $HOME/.config/nvim/init.lua<Enter>", { desc = '' })
--- Tagbar (require ctags)
-vim.keymap.set("n", "<Leader>t",  ":TagbarToggle<CR>", {})
-
--- caw for comment out <C-_> means <C-/>
-vim.keymap.set("n", "<C-_>",     "<plug>(caw:i:toggle)", { desc = '' })
-vim.keymap.set("v", "<C-_>",     "<plug>(caw:i:toggle)", { desc = '' })
-vim.keymap.set("n", "<Leader>/", "<plug>(caw:i:toggle)", { desc = '' })
-vim.keymap.set("v", "<Leader>/", "<plug>(caw:i:toggle)", { desc = '' })
-
-vim.g.auto_save = 1
 vim.g.python3_host_prog = '/home/worker/.virtualenv/v3/bin/python'
 vim.g.python_host_prog = '/home/worker/.virtualenv/v3/bin/python'
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('turn_off_auto_commenting', {}),
+  pattern = '*',
+  command = [[setlocal fo-=cro]]
+})
 
 local options = {
   encoding = "utf-8",
@@ -64,21 +55,46 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.api.nvim_create_autocmd('FileType', {
-  group = vim.api.nvim_create_augroup('turn_off_auto_commenting', {}),
-  pattern = '*',
-  command = [[setlocal fo-=cro]]
-})
-
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+  'doums/darcula',
+  'folke/tokyonight.nvim',
+  {
+    'sjl/badwolf',
+    priority = 1000,
+    config = function ()
+    	vim.cmd.colorscheme 'badwolf'
+    end,
   },
-  config = function ()
-    require('neo-tree').setup {}
-  end,
+  'vim-scripts/sudo.vim',
+  'tpope/vim-surround',
+  {
+    'tyru/caw.vim',
+    config = function ()
+      -- caw for comment out <C-_> means <C-/>
+      vim.keymap.set("n", "<C-_>",     "<plug>(caw:i:toggle)", { desc = '' })
+      vim.keymap.set("v", "<C-_>",     "<plug>(caw:i:toggle)", { desc = '' })
+      vim.keymap.set("n", "<Leader>/", "<plug>(caw:i:toggle)", { desc = '' })
+      vim.keymap.set("v", "<Leader>/", "<plug>(caw:i:toggle)", { desc = '' })
+    end,
+  },
+  {
+  'vim-scripts/vim-auto-save',
+    config = function ()
+      -- auto save
+      vim.g.auto_save = 1
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function ()
+      require('neo-tree').setup {}
+      vim.keymap.set('n', '<leader>n', ":Neotree toggle<CR>", { desc = '[N]eotree toggle' })
+    end,
+  }
 }
