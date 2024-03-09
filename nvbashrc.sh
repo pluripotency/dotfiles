36a3a324f240c7467fa6f5f99a9f8e165e1aba08
Masayoshi Kokuryo <masakoku@gmail.com>
2024-03-09 21:07:15 +0900

    fix: no nvim dir case

--- nvbashrc.sh
+++ nvbashrc.sh
@@ -15,10 +15,12 @@ case ${1} in
     ;;
   * )
     if [ -d ${CURRENT}/${1} ]; then
-      echo "Adding ${1} entry to .bashrc..."
-      LINK="alias ${1}='NVIM_APPNAME=${1} nvim'"
-      grep -q "${LINK}" ${BASHRC} || echo "${LINK}" >> ${BASHRC} 
-      exit
+      if [ ! -z ${1} ]; then
+        echo "Adding ${1} entry to .bashrc..."
+        LINK="alias ${1}='NVIM_APPNAME=${1} nvim'"
+        grep -q "${LINK}" ${BASHRC} || echo "${LINK}" >> ${BASHRC} 
+        exit
+      fi
     fi
     ;;
 esac
@@ -26,4 +28,5 @@ esac
 echo "Adding nvim/vim entry to .bashrc..."
 grep -q "${NVIM_LINK}" ${BASHRC} || echo "${NVIM_LINK}" >> ${BASHRC} 
 grep -q "${VIM_LINK}" ${BASHRC} || echo "# ${VIM_LINK}" >> ${BASHRC} 
+source ${BASHRC}
 
