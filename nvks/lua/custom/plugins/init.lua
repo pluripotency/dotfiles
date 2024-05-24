0d3ebfd1d5fb2acb6742cf078f8a771b8a1d1596
Masayoshi Kokuryo <masakoku@gmail.com>
2024-05-25 08:15:43 +0900

    removed neotree

--- nvks/lua/custom/plugins/init.lua
+++ nvks/lua/custom/plugins/init.lua
@@ -82,18 +82,5 @@ return {
       -- vim.o.autochdir = true
     end,
   }
-  --{
-  --  "nvim-neo-tree/neo-tree.nvim",
-  --  version = "*",
-  --  dependencies = {
-  --    "nvim-lua/plenary.nvim",
-  --    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  --    "MunifTanjim/nui.nvim",
-  --  },
-  --  config = function ()
-  --    require('neo-tree').setup {}
-  --    vim.keymap.set('n', '<leader>n', ":Neotree toggle<CR>", { desc = '[N]eotree toggle' })
-  --  end,
-  --}
 }
 
