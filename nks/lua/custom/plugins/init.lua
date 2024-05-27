32c65ee29f9da10a4232d5ba9ee76b77d940bbba
Masayoshi Kokuryo <masakoku@gmail.com>
2024-05-28 08:43:44 +0900

    none-ls setting in lazy

--- nks/lua/custom/plugins/init.lua
+++ nks/lua/custom/plugins/init.lua
@@ -32,8 +32,37 @@ return {
     --   vim.keymap.set("v", "<Leader>/", "<plug>(caw:i:toggle)", { desc = '' })
     -- end,
   -- },
-  "nvimtools/none-ls.nvim",
-  'jayp0521/mason-null-ls.nvim',
+--  'jayp0521/mason-null-ls.nvim',
+   {
+    "nvimtools/none-ls.nvim",
+    ft = "go",
+    opts = function()
+      local null_ls = require 'null-ls'
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      local opts = {
+        sources = {
+          null_ls.builtins.formatting.gofmt,
+          null_ls.builtins.formatting.goimports,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({
+              group = augroup,
+              buffer = bufnr,
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnf = bufnr })
+              end,
+            })
+          end
+        end,
+      }
+      return opts
+    end,
+  },
   -- {
   --   "jose-elias-alvarez/null-ls.nvim",
   --   ft = "go",
