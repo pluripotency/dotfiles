83a0360f690745ff7c74d2a4d469d7c841601150
Masayoshi Kokuryo <masakoku@gmail.com>
2025-10-19 09:30:26 +0900

    nold is telescope base

--- nold/lua/custom/plugins/init.lua
+++ nold/lua/custom/plugins/init.lua
@@ -3,12 +3,19 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- {
+  --   'airblade/vim-rooter',
+  --   init = function()
+  --     vim.g.rooter_patterns = { '.git', 'pyproject.toml', 'package.json', '!node_modules' }
+  --   end,
+  --   vim.keymap.set('n', '<leader>a', function() end),
+  -- },
   'kchmck/vim-coffee-script',
   'digitaltoad/vim-pug',
 
   -- colorscheme: tokyonight is set by top init.lua by priority 1000
   'navarasu/onedark.nvim',
-  'doums/darcula',
+  -- 'doums/darcula',
   -- {
   --   'folke/tokyonight.nvim',
   --   priority = 1000,
@@ -17,41 +24,74 @@ return {
   --   end,
   -- },
   'vim-scripts/sudo.vim',
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
-    },
-    config = function()
-      require('noice').setup {
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-      }
-    end,
-  },
+  -- {
+  --   'folke/noice.nvim',
+  --   event = 'VeryLazy',
+  --   opts = {
+  --     -- add any options here
+  --   },
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     'MunifTanjim/nui.nvim',
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     'rcarriga/nvim-notify',
+  --   },
+  --   config = function()
+  --     require('noice').setup {
+  --       lsp = {
+  --         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --         override = {
+  --           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+  --           ['vim.lsp.util.stylize_markdown'] = true,
+  --           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+  --         },
+  --       },
+  --       -- views = {
+  --       --   backend = 'notify',
+  --       --   fallback = 'mini',
+  --       --   format = 'notify',
+  --       --   replace = false,
+  --       --   merge = false,
+  --       --   view = 'notify',
+  --       --   position = {
+  --       --     row = -1, -- 画面下端から1行上
+  --       --     col = -1, -- 画面右端から1列左
+  --       --   },
+  --       --   size = {
+  --       --     width = 'auto',
+  --       --     height = 'auto',
+  --       --   },
+  --       --   win_options = {
+  --       --     winblend = 0,
+  --       --   },
+  --       -- },
+  --       views = {
+  --         notify = {
+  --           backend = 'notify',
+  --           anchor = 'SE', -- South-East (右下)
+  --           position = {
+  --             row = '95%', -- 画面の95%の位置
+  --             col = '95%', -- 画面の95%の位置
+  --           },
+  --           size = {
+  --             width = 'auto',
+  --             height = 'auto',
+  --           },
+  --         },
+  --       },
+  --       -- you can enable a preset for easier configuration
+  --       presets = {
+  --         bottom_search = true, -- use a classic bottom cmdline for search
+  --         command_palette = true, -- position the cmdline and popupmenu together
+  --         long_message_to_split = true, -- long messages will be sent to a split
+  --         inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  --         lsp_doc_border = false, -- add a border to hover docs and signature help
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     'folke/trouble.nvim',
     opts = {}, -- for default options, refer to the configuration section for custom setup.
@@ -211,11 +251,12 @@ return {
     config = function()
       require('nvim-tree').setup {
         filters = {
-          git_ignored = false,
-          custom = {
-            '^\\.git',
-            '^node_modules',
-          },
+          dotfiles = false,
+          git_ignored = false, -- デフォルトはtrue
+          -- custom = {
+          --   '^\\.git',
+          --   '^node_modules',
+          -- },
         },
         update_focused_file = {
           enable = true,
