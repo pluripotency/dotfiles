8895e6c099c403b44987dff23d301bd8e47f998e
Masayoshi Kokuryo <masakoku@gmail.com>
2026-01-18 14:15:50 +0900

    fix: path to run

--- plur_dotfiles/run.sh
+++ plur_dotfiles/run.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 CURRENT=$(cd $(dirname $0);pwd)
-bash $CURRENT/install_uv.sh
-# uv python install 3.13
-# uv python pin 3.13
+cd $CURRENT
+bash install_uv.sh
 uv run src/plur_dotfiles/main.py
