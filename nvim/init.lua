48fe014ab81fbfe3dd6d36402b5cad395aae4e4c
Masayoshi Kokuryo <masakoku@gmail.com>
2026-08-01 06:32:14 +0900

    disable ty autoimport

--- nvim/init.lua
+++ nvim/init.lua
@@ -295,6 +295,15 @@ require('lazy').setup({
           end,
         },
       }
+      vim.lsp.config('ty', {
+        settings = {
+          ty = {
+            completion = {
+              autoImport = false,
+            }
+          },
+        },
+      })
       vim.lsp.config('lua_ls', {
           settings = {
             Lua = {
@@ -533,57 +542,7 @@ require('lazy').setup({
     lazy = false,
     branch = 'main',
     build = ':TSUpdate',
-    init = function ()
-      vim.api.nvim_create_autocmd('FileType', {
-        callback = function ()
-          pcall(vim.treesitter.start)
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end,
-    -- config = function ()
-    --   require('nvim-treesitter').setup {}
-    -- end
   },
-  -- { -- Highlight, edit, and navigate code
-  --   'nvim-treesitter/nvim-treesitter',
-  --   lazy = false,
-  --   build = ':TSUpdate',
-  --   main = 'nvim-treesitter.config', -- Sets main module to use for opts
-  --   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  --   opts = {
-  --     ensure_installed = {
-  --       'bash',
-  --       'c',
-  --       'python',
-  --       'diff',
-  --       'html',
-  --       'lua',
-  --       'luadoc',
-  --       'markdown',
-  --       'markdown_inline',
-  --       'query',
-  --       'vim',
-  --       'vimdoc'
-  --     },
-  --     -- Autoinstall languages that are not installed
-  --     auto_install = true,
-  --     highlight = {
-  --       enable = true,
-  --       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-  --       --  If you are experiencing weird indenting issues, add the language to
-  --       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-  --       additional_vim_regex_highlighting = { 'ruby' },
-  --     },
-  --     indent = { enable = true, disable = { 'ruby', 'python' } },
-  --   },
-  --   -- There are additional nvim-treesitter modules that you can use to interact
-  --   -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --   --
-  --   --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --   --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  -- },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
